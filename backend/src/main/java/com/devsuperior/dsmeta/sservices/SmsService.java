d5f36c9a3e88f7b57f9c0a87d54f6746fee7655f
CrisMai <crismai.dev@gmail.com>
2022-09-09 11:12:11 -0300

    Date update

--- backend/src/main/java/com/devsuperior/dsmeta/sservices/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/sservices/SmsService.java
@@ -29,7 +29,7 @@ public class SmsService {
 	private SaleRepository saleRepository;
 
 	public void sendSms(Long saleId) {
-		
+		System.out.println("test" + twilioKey );
 		Sale sale = saleRepository.findById(saleId).get();
 		
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
